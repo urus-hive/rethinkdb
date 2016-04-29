1aed050de90b07006420b86813377105379cd141
Daniel Mewes <daniel@rethinkdb.com>
2016-04-29 11:42:37 -0700

    Improved OpenSSL error messages.

--- src/crypto/error.cc
+++ src/crypto/error.cc
@@ -17,6 +17,16 @@ openssl_error_category_t::openssl_error_category_t() {
     switch (condition) {
         case 336027804:
             return "Received a plain HTTP request on the HTTPS port.";
+        case 336027900:
+            return "Unknown protocol. By default, RethinkDB requires at least TLSv1.2. "
+                   "You can enable older TLS versions on the server through the "
+                   "`--tls-min-protocol TLSv1` option. (OpenSSL error 336027900)";
+        case 336109761:
+            return "No shared cipher. You can change the list of enabled ciphers on the "
+                   "server through the `--tls-ciphers` option. Older OpenSSL clients "
+                   "(e.g. on OS X) might require `--tls-ciphers "
+                   "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH:AES256-SHA`. "
+                   "(OpenSSL error 336109761)";
         default:
             char const *message = ERR_reason_error_string(condition);
             return strprintf("%s (OpenSSL error %d)",
