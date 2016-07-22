a8ab48cfc7a29b3289c3704690030725fa60653e
Etienne Laurin <atnnn@rethinkdb.com>
2016-07-22 09:44:18 -0700

    avoid negative index in debugf

--- src/debug.cc
+++ src/debug.cc
@@ -79,7 +79,7 @@ void debugf(const char *msg, ...) {
 
     va_end(ap);
 
-    if (buf.c_str()[buf.size() - 1] != '\n') {
+    if (!buf.size() || buf.c_str()[buf.size() - 1] != '\n') {
         buf.appendf("\n");
     }
 
