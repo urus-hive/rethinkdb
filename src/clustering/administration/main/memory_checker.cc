8a6374825895fa4e30613ddc129d246ffabb0fb9
Nighelles <nighelles@rethinkdb.com>
2016-08-15 15:27:21 -0700

    Swap checking is finished.

--- src/clustering/administration/main/memory_checker.cc
+++ src/clustering/administration/main/memory_checker.cc
@@ -37,48 +37,32 @@ void memory_checker_t::do_check(UNUSED auto_drainer_t::lock_t keepalive) {
     int res = getrusage(RUSAGE_SELF, &current_usage);
 
     if (res == -1) {
-        // handle error?
-        logERR("Error getting major page faults: %d\n", errno);
+        logERR("Error in swap usage detection: %d\n", errno);
     }
     size_t new_swap_usage = current_usage.ru_majflt;
 #endif
 
-    // This is because mach won't give us the swap used by our process.
     if (first_check == practice_runs) {
         swap_usage = new_swap_usage;
         first_check = false;
     }
 
-#if defined(__MACH__)
     const std::string error_message =
-        "Data from a process on this server"
-        " has been placed into swap memory in the past ten minutes."
-        " If the data is from RethinkDB, this may impact performance.";
-#else
-    const std::string error_message =
-        "Some RethinkDB data on this server"
-        " has been placed into swap memory in the past ten minutes."
-        " This may impact performance.";
-#endif
+        "RethinkDB has been accessing a lot of swap memory in the past ten"
+        " minutes. This may impact performace.";
 
     if (new_swap_usage > swap_usage + 200 && first_check == 0) {
         // We've started using more swap
         if (print_log_message) {
-#if defined(__MACH__)
-            logWRN("Data from a process on this server"
-                   " has been placed into swap memory."
-                   " If the data is from RethinkDB, this may impact performance.");
-#else
-            logWRN("Some RethinkDB data on this server"
-                   " has been placed into swap memory."
-                   " This may impact performance.");
-#endif
+            logWRN("RethinkDB has been accessing a lot of swap memory in the past ten"
+                   " minutes. This may impact performace.");
+
             print_log_message = false;
         }
         refresh_timer = reset_checks;
         memory_issue_tracker.report_error(error_message);
     } else if (refresh_timer == 0) {
-        // We haven't put anything in swap for 10 min.
+        // We haven't had more than 200 major page faults per minute for the last 10m.
         memory_issue_tracker.report_success();
         print_log_message = true;
     }
