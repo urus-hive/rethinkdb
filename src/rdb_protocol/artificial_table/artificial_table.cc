63576f1d421f1f2a993cfd5d1d86e321e83b674c
Nighelles <nighelles@rethinkdb.com>
2016-09-14 16:36:35 -0700

    fixes

--- src/rdb_protocol/artificial_table/artificial_table.cc
+++ src/rdb_protocol/artificial_table/artificial_table.cc
@@ -138,6 +138,7 @@ scoped_ptr_t<ql::reader_t> artificial_table_t::read_all_with_sindexes(
 
     std::vector<ql::datum_t> items_vector;
 
+    guarantee(items.get_type() == ql::datum_t::type_t::R_ARRAY);
     for (size_t i = 0; i < items.arr_size(); ++i) {
         items_vector.push_back(std::move(items.get(i)));
     }
